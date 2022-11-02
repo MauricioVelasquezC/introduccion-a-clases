37276531c8adab0b93fd2d6199197fac45c23351
Mauricio Velasquez <114424920+MauricioVelasquezC@users.noreply.github.com>
2022-11-02 17:38:17 -0500

    Add files via upload

---
+++ Ejemplos_Clases.py
@@ -0,0 +1,46 @@
+class Persona:
+    def __init__(self):        
+        self.bigote=""
+        self.nombre=""
+        self.edad=""
+        self.genero=""
+        self.volumen="" 
+     
+    def bigote (self):
+        pass
+        
+    def nombre (self):
+        pass
+        
+    def edad (self):
+        pass
+        
+    def genero (self):  
+        pass
+        
+    def Volumen (self):    
+        pass
+              
+Sujeto_a=Persona()
+
+Sujeto_a.bigote ="False"
+Sujeto_a.nombre="Camilo"
+Sujeto_a.edad="20"
+Sujeto_a.genero="Pansexual"
+Sujeto_a.Volumen="Alto"             
+            
+Sujeto_b=Persona()
+
+Sujeto_b.bigote ="False"
+Sujeto_b.nombre="Cristian"
+Sujeto_b.edad="56"
+Sujeto_b.genero="masculino"
+Sujeto_b.Volumen="medio" 
+
+Sujeto_c=Persona()
+
+Sujeto_c.bigote ="False"
+Sujeto_c.nombre="Lina"
+Sujeto_c.edad="20"
+Sujeto_c.genero="Femenino"
+Sujeto_c.Volumen="bajo"             
